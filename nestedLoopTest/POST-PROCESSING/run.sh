93689437f086b7008689b367890b3e17e7befd81
antoniopucciarelli <antonio.pucciarelli@gmail.com>
2022-06-16 17:43:13 +0200

    changes on postProcessing, grid adaptivity, thermal analysis

--- nestedLoopTest/POST-PROCESSING/run.sh
+++ nestedLoopTest/POST-PROCESSING/run.sh
@@ -2,9 +2,12 @@
 
 rm -r grid_output *.pvd
 
-my_pvpython=/home/raffaele/ParaView-5.10.0-MPI-Linux-Python3.9-x86_64/bin/pvpython
+#my_pvpython=/home/raffaele/ParaView-5.10.0-MPI-Linux-Python3.9-x86_64/bin/pvpython
 
-$my_pvpython grid2paraview.py grid_descriptor.txt grid_output -r ../fField/flow_field.*
+
+#$my_pvpython grid2paraview.py grid_descriptor.txt grid_output -r ../fField/flow_field.*
+
+my_pvpython grid2paraview.py grid_descriptor.txt grid_output -r ../fField.flow_field.*
 
 #paraview *.pvd 
 
